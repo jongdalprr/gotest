2ca04605b4378a1444b45580e2782c85bafba4f2
jongdalprr <jeremi222@naver.com>
2025-06-27 16:00:36 +0900

    Add files via upload

---
+++ api/train.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const API_KEY = '73646e69466a657234366f45705247';
+  const url = `http://swopenAPI.seoul.go.kr/api/subway/${API_KEY}/json/realtimeStationArrival/0/50/서울`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: "데이터를 불러올 수 없습니다.", detail: e.message });
+  }
+}
